50e37989a33a7b1898849bd0181bda0bac777e53
Hidayat Lossen <dayatlossentuasikal@gmail.com>
2026-05-22 20:10:52 +0700

    chore(config): align env, lint, and prisma setup

--- eslint.config.js
+++ eslint.config.js
@@ -1,12 +1,18 @@
 import globals from 'globals';
 import pluginJs from '@eslint/js';
 import daStyle from 'eslint-config-dicodingacademy';
- 
+
 export default [
   { ignores: ['src/generated/**'] },
   daStyle,
   { files: ['**/*.js'], languageOptions: { sourceType: 'module' } },
   { languageOptions: { globals: globals.node } },
   pluginJs.configs.recommended,
+  {
+    rules: {
+      camelcase: 'off',
+      'linebreak-style': 'off',
+      'no-console': 'off',
+    },
+  },
 ];
- 
